5678474ffc2d41e042745881b635390ce5b0e708
Ayush Saxena <ayush.builds.001@gmail.com>
2026-08-01 08:08:15 +0530

    Delete Difficulty: Basic directory

--- Difficulty: Basic/While Loop/while-loop.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public static void utility(int x) {
-        // code here
-         while (x >= 0) {
-            System.out.print(x + " ");
-            x--;
-        }
-    }
-}
